ad782330cdf8e8a9f4c3317f1392826d5ba62b30
maehara08 <aso.riiiku08@gmail.com>
2018-07-31 21:11:50 +0900

    Create login activity.

--- app/src/main/java/maehara/riku/com/scienceday2018_twitter/LoginActivity.java
+++ app/src/main/java/maehara/riku/com/scienceday2018_twitter/LoginActivity.java
@@ -1,13 +1,51 @@
 package maehara.riku.com.scienceday2018_twitter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TwitterLoginButton twitterLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+        if (session == null) {
+            twitterLoginButton = findViewById(R.id.loginButton);
+            twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+                @Override
+                public void success(Result<TwitterSession> result) {
+
+                }
+
+                @Override
+                public void failure(TwitterException exception) {
+
+                }
+            });
+        } else {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+//        TwitterAuthToken authToken = session.getAuthToken();
+//        String token = authToken.token;
+//        String secret = authToken.secret;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        twitterLoginButton.onActivityResult(requestCode, requestCode, data);
     }
 }
