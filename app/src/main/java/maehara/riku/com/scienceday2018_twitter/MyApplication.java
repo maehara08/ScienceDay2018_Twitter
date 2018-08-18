6ceeec476b665935bf6fc9baada17666746e963f
maehara08 <aso.riiiku08@gmail.com>
2018-08-19 00:32:21 +0900

    Add twitter init.

--- app/src/main/java/maehara/riku/com/scienceday2018_twitter/MyApplication.java
+++ app/src/main/java/maehara/riku/com/scienceday2018_twitter/MyApplication.java
@@ -1,14 +1,23 @@
 package maehara.riku.com.scienceday2018_twitter;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.twitter.sdk.android.core.DefaultLogger;
 import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
 
 public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Twitter.initialize(this);
+        TwitterConfig twitterConfig = new TwitterConfig.Builder(this)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig(BuildConfig.CONSUMER_KEY, BuildConfig.CONSUMER_SECRET))
+                .debug(true)
+                .build();
+        Twitter.initialize(twitterConfig);
     }
 }
